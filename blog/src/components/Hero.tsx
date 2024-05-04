f6a487853244c6faa65a431cb33997bcd7e3a46c
Asif <babu033045@gmail.com>
2024-05-04 14:56:33 +0600

    Update code

--- blog/src/components/Hero.tsx
+++ blog/src/components/Hero.tsx
@@ -1,8 +1,6 @@
 const Hero = ({ tag, title, subtitle }) => {
   return (
-    <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center rounded-lg h-auto">
-      Asif
-    </div>
+    <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center rounded-lg h-auto"></div>
   );
 };
 
